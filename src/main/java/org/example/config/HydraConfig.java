ee4e8b366d3e266373293da4104e0343bbd30215
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2026-05-21 19:08:22 +0530

    '

--- src/main/java/org/example/config/HydraConfig.java
+++ src/main/java/org/example/config/HydraConfig.java
@@ -1,4 +1,23 @@
 package org.example.config;
 
+import org.yaml.snakeyaml.Yaml;
+
+import java.util.Map;
+
+import java.io.InputStream;
+
 public class HydraConfig {
+
+    private final Map<String, Object> data;
+
+    public HydraConfig() {
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("hydra.yaml");
+        if (inputStream == null) throw new IllegalStateException("hydra.yaml not found on classpath");
+        Yaml yaml = new Yaml();
+        this.data = yaml.load(inputStream);
+    }
+
+    public Map<String, Object> getData(){
+        return data;
+    }
 }
