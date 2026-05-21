fcf03667b1ca549644a14bb15718aea5334d2468
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2026-05-21 19:42:21 +0530

    feat(flush): add memtable cleaner skeleton

--- src/main/java/org/example/storage/flush/MemtableCleaner.java
+++ src/main/java/org/example/storage/flush/MemtableCleaner.java
@@ -1,4 +1,9 @@
 package org.example.storage.flush;
 
+import java.util.concurrent.Future;
+
 public class MemtableCleaner {
+    public Future<?> flushLargestMemtable(){
+        return null;
+    }
 }
