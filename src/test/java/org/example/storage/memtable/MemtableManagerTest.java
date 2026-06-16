8165a39f682c2f4552a094da3c8762e98dfe6f1e
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2026-06-16 15:51:45 +0530

    add test for memtable Manager

--- src/test/java/org/example/storage/memtable/MemtableManagerTest.java
+++ src/test/java/org/example/storage/memtable/MemtableManagerTest.java
@@ -1,4 +1,14 @@
 package org.example.storage.memtable;
 
+import org.example.storage.bootstrap.StorageInitilizer;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutionException;
+
 public class MemtableManagerTest {
+    @Test
+    public void test () throws ExecutionException, InterruptedException {
+        StorageInitilizer initilizer = new StorageInitilizer();
+
+    }
 }
