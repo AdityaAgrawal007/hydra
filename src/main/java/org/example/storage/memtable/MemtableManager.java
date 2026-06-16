cb2c51cc34571f53869d657be56c54b70fc9bb57
AdityaSMAgrawal <adityasmagrawal@gmail.com>
2026-06-16 15:50:07 +0530

    add central manger for all memtables

--- src/main/java/org/example/storage/memtable/MemtableManager.java
+++ src/main/java/org/example/storage/memtable/MemtableManager.java
@@ -2,26 +2,26 @@ package org.example.storage.memtable;
 
 /*
  * responsibilities of this class are:
- * maintain active memtable
- * switch memtables when current one becomes full
- * create new memtables using configured factory
+ * maintain active memtable - done
+ * switch memtables when current one becomes full - done
+ * create new memtables using configured factory - done
  * pair memtables with allocators
- * trigger flush for immutable memtables
+ * trigger flush for immutable memtables - done
  */
 
 import org.example.storage.flush.MemtableCleaner;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class MemtableManager {
-    Memtable.Factory factory;
-    Map<String, AtomicReference<Memtable>> registry;
+    public final Map<String, AtomicReference<Memtable>> registry;
 
-    public MemtableManager(Memtable.Factory factory) {
-        this.factory = factory;
+    public MemtableManager(Map<String, AtomicReference<Memtable>> registry) {
+        this.registry = registry;
     }
 
     // the memtable directly takes in the Factory (i.e. the object that knows how to create a certian type of memtable) and then just
@@ -29,20 +29,47 @@ public class MemtableManager {
     //    public Memtable createMemtable() {
     //        return factory.create();
     //    }
+    private String getLargestMemtableKey() {
+        String largestKey = null;
+        long maxSize = 0;
 
-    boolean switchMemtable() throws ExecutionException, InterruptedException {
-        Memtable newMemtable = factory.create(); // 1.5 create a new active memtable
-
-        AtomicReference<Memtable> largestMemtable = null;
-        long maxSpace = 0;
         for (Map.Entry<String, AtomicReference<Memtable>> entry : registry.entrySet()) {
-            AtomicReference<Memtable> currentMemtable = entry.getValue();
-            if (currentMemtable.get().size() >= maxSpace) {
-                largestMemtable = currentMemtable;
-                maxSpace = currentMemtable.get().size();
+            long size = entry.getValue().get().size();
+
+            if (size > maxSize) {
+                maxSize = size;
+                largestKey = entry.getKey();
             }
         }
 
+        return largestKey;
+    }
+
+    boolean switchMemtable() throws Exception{
+//        Memtable newMemtable = factory.create(); // 1.5 create a new active memtable
+
+        String largestKey = getLargestMemtableKey();
+
+        if (largestKey == null) {
+            return false;
+        }
+        AtomicReference<Memtable> largestMemtable = registry.get(largestKey);
+//        AtomicReference<Memtable> largestMemtable = null;
+//        long maxSpace = 0;
+//        for (Map.Entry<String, AtomicReference<Memtable>> entry : registry.entrySet()) {
+//            AtomicReference<Memtable> currentMemtable = entry.getValue();
+//            if (currentMemtable.get().size() >= maxSpace) {
+//                largestMemtable = currentMemtable;
+//                maxSpace = currentMemtable.get().size();
+//            }
+//        }
+
+        String factoryClassName = "org.example.storage.memtable." + largestKey + "Factory";
+        Class<?> clazz = Class.forName(factoryClassName);
+
+        Memtable.Factory factory = (Memtable.Factory) clazz.getDeclaredConstructor().newInstance();
+
+        Memtable newMemtable = factory.create();
         AtomicReference<Memtable> oldLargestMemtable = largestMemtable;
         largestMemtable.get().setImmutable(true); // 1. lock the memtable
 
